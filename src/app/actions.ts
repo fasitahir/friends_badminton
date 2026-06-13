8d0dd0adcaa2f5894a457c2f7343bef3f52bceef
fasitahir <fasitahir2019@gmail.com>
2026-06-14 01:21:05 +0500

    login fixed

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -41,7 +41,6 @@ export async function login(formData: FormData) {
 export async function logout() {
   const cookieStore = await cookies();
   cookieStore.delete("admin_auth");
-  return { success: true };
 }
 import {
   playerSchema,
